15de18efba65312dd8113f53f83a5ad45d794177
yjb23tech <yjb23tech@gmail.com>
2023-12-02 12:15:53 +0000

    Hahah

--- components/TravelCard.jsx
+++ components/TravelCard.jsx
@@ -1,9 +1,9 @@
-
+import travel_card_photo from "../public/images/japan.jpeg"
 
 const TravelCard = () => {
     return (
-        <div>
-
+        <div className="travel-card">
+            <img src={travel_card_photo} />
         </div>
     )
 };

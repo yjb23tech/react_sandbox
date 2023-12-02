0e63cb5e5b924c8c38895d0a555a2c14d5dd32d9
yjb23tech <yjb23tech@gmail.com>
2023-12-02 16:36:17 +0000

    Introducing Props

--- App.jsx
+++ App.jsx
@@ -6,9 +6,9 @@ const App = () => {
     return (
         <div>
             <NavigationBar />
-            <TravelCard />
-            <TravelCard />
-            <TravelCard />
+            <TravelCard country_name="JAPAN" attraction_name="Mount Fuji" travel_dates="12 Jan, 2021 - 24 Jan, 2021"/>
+            <TravelCard country_name="AUSTRALIA" attraction_name="Sydney Opera House" travel_dates="27 May, 2021 - 8 Jun, 2021"/>
+            <TravelCard country_name="NORWAY" attraction_name="Geirangerfjord" travel_dates="01 Oct, 2021 - 18 Nov, 2021" />
         </div>
     )
 };
--- components/TravelCard.jsx
+++ components/TravelCard.jsx
@@ -1,18 +1,18 @@
 import travel_card_photo from "../public/images/japan.jpeg"
 import travel_pin_icon from "../public/images/location_icon.png"
 
-const TravelCard = () => {
+const TravelCard = (props) => {
     return (
         <div className="travel-card">
             <img src={travel_card_photo} className="travel-card-photo"/>
             <div className="travel-card-content">
                 <div className="location-details">
                     <img src={travel_pin_icon} />
-                    <span>JAPAN</span>
+                    <span>{props.country_name}</span>
                     <span className="google-maps-link"><a href="https://www.google.com/maps/d/u/0/viewer?mid=1W9AtcwnYAqP1yVHr5YjhwmQa0eM&hl=en&ll=35.36479441415821%2C138.763048&z=14">View on Google Maps</a></span>
                 </div>
-                <h1 className="location-title">Mount Fuji</h1>
-                <p className="location-dates">12 Jan, 2021 - 24 Jan, 2021</p>
+                <h1 className="location-title">{props.attraction_name}</h1>
+                <p className="location-dates">{props.travel_dates}</p>
                 <div className="location-text-box">
                     <p>
                     Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters 

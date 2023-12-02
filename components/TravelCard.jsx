7c7604e022b66abd71e45ac443eebf6abcc71697
yjb23tech <yjb23tech@gmail.com>
2023-12-02 14:21:08 +0000

    Doing a lot lol

--- components/TravelCard.jsx
+++ components/TravelCard.jsx
@@ -9,8 +9,14 @@ const TravelCard = () => {
                 <div className="location-details">
                     <img src={travel_pin_icon} />
                     <span>JAPAN</span>
-                    <span>View on Google Maps</span>
+                    <span className="google-maps-link"><a href="https://www.google.com/maps/d/u/0/viewer?mid=1W9AtcwnYAqP1yVHr5YjhwmQa0eM&hl=en&ll=35.36479441415821%2C138.763048&z=14">View on Google Maps</a></span>
                 </div>
+                <h1 className="location-title">Mount Fuji</h1>
+                <p className="location-dates">12 Jan, 2021 - 24 Jan, 2021</p>
+                <p>
+                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). 
+                    Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                </p>
             </div>
         </div>
     )

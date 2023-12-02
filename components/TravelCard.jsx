1fc3ca9dc2f898f1a6426aed34e8a76079c78375
yjb23tech <yjb23tech@gmail.com>
2023-12-02 15:38:48 +0000

    Funky

--- components/TravelCard.jsx
+++ components/TravelCard.jsx
@@ -13,10 +13,13 @@ const TravelCard = () => {
                 </div>
                 <h1 className="location-title">Mount Fuji</h1>
                 <p className="location-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p>
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). 
-                    Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
-                </p>
+                <div className="location-text-box">
+                    <p>
+                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters 
+                    (12,380 feet). Mount Fuji is the single most popular tourist 
+                    site in Japan, for both Japanese and foreign tourists.
+                    </p>
+                </div>
             </div>
         </div>
     )

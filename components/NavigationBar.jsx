eb6225da9eb9562a0c954f6b8d1e56df0a2c9570
yjb23tech <yjb23tech@gmail.com>
2023-12-02 10:54:52 +0000

    Building slowly

---
+++ components/NavigationBar.jsx
@@ -0,0 +1,12 @@
+
+import globe_icon from "../public/images/globe.png"
+
+const NavigationBar = () => {
+    return (
+        <nav>
+            <img src={globe_icon} />
+        </nav>
+    )
+}
+
+export default NavigationBar;

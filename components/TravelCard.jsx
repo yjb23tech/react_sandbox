a7f75afbcb9121f1cb17106029e3359524f1377d
yjb23tech <yjb23tech@gmail.com>
2023-12-02 12:39:44 +0000

    Screamer

--- components/TravelCard.jsx
+++ components/TravelCard.jsx
@@ -1,9 +1,17 @@
 import travel_card_photo from "../public/images/japan.jpeg"
+import travel_pin_icon from "../public/images/location_icon.png"
 
 const TravelCard = () => {
     return (
         <div className="travel-card">
             <img src={travel_card_photo} className="travel-card-photo"/>
+            <div className="travel-card-content">
+                <div className="location-details">
+                    <img src={travel_pin_icon} />
+                    <span>JAPAN</span>
+                    <span>View on Google Maps</span>
+                </div>
+            </div>
         </div>
     )
 };
